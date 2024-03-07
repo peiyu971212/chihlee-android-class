16e7569c72f9bd1fc341f2aa9b1e9e7a8cbe8eef
peiyu <peiyu971212@gmail.com>
2024-03-08 00:31:29 +0800

    upload 20240304_game_listener_1

---
+++ 20240304_game_listener_1/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "20240304_game_listener_1"
+include(":app")
+ 
