841dfc5e72838deeccbb3f5315dda19dee354f5b
peiyu <peiyu971212@gmail.com>
2024-04-14 20:50:59 +0800

    add ShopApp

--- ShopApp/app/src/main/java/com/example/shopapp/LoginActivity.kt
+++ ShopApp/app/src/main/java/com/example/shopapp/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.shopapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,10 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, ShopActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
