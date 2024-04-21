267b2cb1357908996ca28204f374484aa11deae4
peiyu <peiyu971212@gmail.com>
2024-04-21 20:49:36 +0800

    add 20240422 Fragment

---
+++ Fragment/app/src/main/java/com/example/fragment/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.fragment
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        //建立 FragmentPagerAdapter 物件
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        //連接 Adapter，讓畫面(Fragment)與 ViewPager 建立關聯
+        findViewById<ViewPager>(R.id.viewPager).adapter = adapter
+    }
+}
