bb92c4be3a9d9071ee41510f262cd93adbcfe986
penny liu <peiyu971212@gmail.com>
2025-05-27 19:45:52 +0800

    product detail

---
+++ ShopDemoApp/app/src/main/java/com/example/shopdemoapp/ui/product/ProductDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shopdemoapp.ui.product
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shopdemoapp.R
+
+class ProductDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+    }
+}
